9f8e5bcea09ac6f9da19b1fd491bb821b6b23f7c
kashishbehl0310 <kashishbehl36@gmail.com>
2018-09-27 18:56:02 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>hello world</Text>
+        <Text style={styles.welcome}>Welcome to Taxi Booing App</Text>
       </View>
     );
   }
