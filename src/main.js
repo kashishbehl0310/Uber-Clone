4a38aa134819c6faebb80a1df573eee38defbae4
kashishbehl0310 <kashishbehl36@gmail.com>
2018-09-27 22:58:05 +0530

    Setting up redux

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
 import React from 'react'
-import {View, Text} from 'react-native'
-
-class Main extends React.Component{
-    render(){
-        return(
-            <View>
-                <Text>Hey Taxi App!</Text>
-            </View>
-        )
-    }
-}
-
-export default Main;
