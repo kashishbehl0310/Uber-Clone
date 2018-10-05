01212d2a89b792ff35164a6ef9b912f3276fee65
kashishbehl0310 <kashishbehl36@gmail.com>
2018-10-05 22:41:56 +0530

    react native maps

---
+++ src/routes/home/components/MapContainer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View } from "native-base";
+import MapView from "react-native-maps";
+
+import styles from './MapContainerStyles'
+
+export const MapContainer = ({region}) => {
+    return(
+        <View style={styles.container}>
+            <MapView
+            provider={MapView.PROVIDER_GOOGLE}
+            style={styles.map}
+            region={region}>
+                
+            </MapView>
+        </View>
+    )
+}
