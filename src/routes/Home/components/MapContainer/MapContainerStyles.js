7cafc99e504552766be0074144d93270871a37ef
kashishbehl0310 <kashishbehl36@gmail.com>
2018-10-14 10:59:37 +0530

    Integrating maps

--- src/routes/Home/components/MapContainer/MapContainerStyles.js
+++ src/routes/Home/components/MapContainer/MapContainerStyles.js
@@ -1,15 +1,16 @@
 import { StyleSheet } from "react-native";
 
 const styles = {
-    containe: {
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
     },
     map:{
-        flex: 1, 
-        ...StyleSheet.absoluteFillObject
-    }
+        flex: 1,
+        height: '100%',
+        width: '100%',
+      }
 }
 
 export default styles;
--- src/routes/Home/components/MapContainer/index.js
+++ src/routes/Home/components/MapContainer/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { View } from "native-base";
-import MapView from "react-native-maps";
+import MapView , {PROVIDER_GOOGLE} from "react-native-maps";
 import styles from "./MapContainerStyles";
 
 export const MapContainer = ({region}) => {
+    console.log(region)
     return(
         <View style={styles.container}>
             <MapView
-                provider={MapView.PROVIDER_GOOGLE}
+                provider={PROVIDER_GOOGLE}
                 style={styles.map}
                 region={region}
             >
-
+                <MapView.Marker
+                    coordinate={region}
+                    pinColor="green"
+                />
             </MapView>
         </View>
     )
