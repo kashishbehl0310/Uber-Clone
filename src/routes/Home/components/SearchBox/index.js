6b284a6b5ffe1fc794945836d110af3bc55efbc9
kashishbehl0310 <kashishbehl36@gmail.com>
2018-10-14 23:59:43 +0530

    UI changes

--- src/routes/Home/components/SearchBox/index.js
+++ src/routes/Home/components/SearchBox/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import {  Text } from "react-native";
 import { View, InputGroup, Input } from "native-base";
 import styles from "./SearchBoxStyles";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export const SearchBox = () => {
     return(
         <View style={styles.searchBox}>
             <Text style={styles.label}>Pick Up</Text>
             <InputGroup>
+                <Icon name="search" size={15} color="#FF5E3A" />
                 <Input style={styles.inputSearch} placeholder="Choose Pick Up location" />
             </InputGroup>
         </View>
