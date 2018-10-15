867e4e38c57a01083cc69f8a5587ca0d28d45d45
kashishbehl0310 <kashishbehl36@gmail.com>
2018-10-15 22:43:59 +0530

    Search results autocomplete

---
+++ src/routes/Home/components/SearchResults/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { View, InputGroup, Input } from "native-base";
+
+import styles from "./SearchResultsStyles";
+
+export const SearchResults = () => {
+
+};
+
+export default SearchResults;
