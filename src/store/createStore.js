873884bfc9824b8a8156a4354964a9ecaac8296e
kashishbehl0310 <kashishbehl36@gmail.com>
2018-12-16 18:51:30 +0530

    Integrating socket on client

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,11 +2,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import makeRootReducer from "./reducers";
 import { createLogger } from "redux-logger";
+import createSocketIoMiddleware from 'redux-socket.io'
+// import io from 'socket.io';
+
+let socket = io('http://localhost:7777', {jsonp: false})
+let SocketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
 
 const log = createLogger({diff: true, collapsed: true});
 
 export default (initialState = {}) =>{
-    const middleware = [thunk, log];
+    const middleware = [thunk, log, SocketIoMiddleware];
 
     const enhancers = [];
 
